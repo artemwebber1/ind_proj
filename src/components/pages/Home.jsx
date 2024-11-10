1afb6507dc3ef7450fd486f6000bcc52b42bfc0a
Artem Chekhonin <chekhonin.artem@gmail.com>
2024-11-11 00:25:55 +0300

    Закончена вёрстка шаблона

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,16 @@
+import img from '../../img/school1.png'
+import s from '../../styles/home.module.css'
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className={s.homeWrapper}>
+            <center>
+                <img src={img} alt="Фото школы" className={s.schoolPhoto} />
+            </center>
+            <p className={s.text}>Приветствуем посетителей официального сайта!</p>
+            <div className={s.usefulLinks}>
+                
+            </div>
         </div>
     )
 }
